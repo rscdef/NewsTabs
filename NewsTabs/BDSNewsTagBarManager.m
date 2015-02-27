de17eef8de0ba6d13a06312f0bf93024c1bfc8c9
rscdef <rscdef@gmail.com>
2015-02-27 19:13:59 +0800

    bug needs fixing

--- NewsTabs/BDSNewsTagBarManager.m
+++ NewsTabs/BDSNewsTagBarManager.m
@@ -74,6 +74,43 @@
         sLastPageIndex = newIndex;
     }
 }
+/* bug needs fixing
+ 2015-02-27 17:20:19.840 BDStockClient[2151:234573] offset: 1.948437 sLastPageIndex: 2 [1,2]
+ 2015-02-27 17:20:19.853 BDStockClient[2151:234573] offset: 1.862500 sLastPageIndex: 2 [1,2]
+ 2015-02-27 17:20:19.870 BDStockClient[2151:234573] offset: 1.743750 sLastPageIndex: 2 [1,2]
+ 2015-02-27 17:20:19.886 BDStockClient[2151:234573] offset: 1.639063 sLastPageIndex: 2 [1,2]
+ 2015-02-27 17:20:19.902 BDStockClient[2151:234573] offset: 1.557812 sLastPageIndex: 2 [1,2]
+ 2015-02-27 17:20:19.948 BDStockClient[2151:234573] offset: 1.481250 sLastPageIndex: 2 [1,2]
+ 2015-02-27 17:20:19.950 BDStockClient[2151:234573] offset: 1.481250 sLastPageIndex: 2 [1,2]
+ 2015-02-27 17:20:19.955 BDStockClient[2151:234573] offset: 1.425000 sLastPageIndex: 2 [1,2]
+ 2015-02-27 17:20:19.957 BDStockClient[2151:234573] offset: 1.387500 sLastPageIndex: 2 [1,2]
+ 2015-02-27 17:20:20.094 BDStockClient[2151:234573] offset: 1.432812 sLastPageIndex: 2 [1,2]
+ 2015-02-27 17:20:20.113 BDStockClient[2151:234573] offset: 1.492188 sLastPageIndex: 2 [1,2]
+ 2015-02-27 17:20:20.161 BDStockClient[2151:234573] offset: 1.582813 sLastPageIndex: 2 [1,2]
+ 2015-02-27 17:20:20.163 BDStockClient[2151:234573] offset: 1.582813 sLastPageIndex: 2 [1,2]
+ 2015-02-27 17:20:20.168 BDStockClient[2151:234573] offset: 1.709375 sLastPageIndex: 2 [1,2]
+ 2015-02-27 17:20:20.171 BDStockClient[2151:234573] offset: 1.892187 sLastPageIndex: 2 [1,2]
+ 
+ 2015-02-27 17:20:20.174 BDStockClient[2151:234573] offset: 2.112500 sLastPageIndex: 2 [2,3]
+ 2015-02-27 17:20:20.321 BDStockClient[2151:234573] offset: 2.179688 sLastPageIndex: 2 [2,3]
+ 2015-02-27 17:20:20.343 BDStockClient[2151:234573] offset: 2.323437 sLastPageIndex: 2 [2,3]
+ 2015-02-27 17:20:20.361 BDStockClient[2151:234573] offset: 2.451562 sLastPageIndex: 2 [2,3]
+ 2015-02-27 17:20:20.380 BDStockClient[2151:234573] offset: 2.562500 sLastPageIndex: 2 [2,3]
+ 2015-02-27 17:20:20.382 BDStockClient[2151:234573] offset: 2.562500 sLastPageIndex: 2 [2,3]
+ 2015-02-27 17:20:20.419 BDStockClient[2151:234573] offset: 2.740625 sLastPageIndex: 2 [2,3]
+ 2015-02-27 17:20:20.431 BDStockClient[2151:234573] offset: 2.803125 sLastPageIndex: 2 [2,3]
+ 2015-02-27 17:20:20.442 BDStockClient[2151:234573] offset: 2.853125 sLastPageIndex: 2 [2,3]
+ 2015-02-27 17:20:20.453 BDStockClient[2151:234573] offset: 2.892188 sLastPageIndex: 2 [2,3]
+ 2015-02-27 17:20:20.470 BDStockClient[2151:234573] offset: 2.923438 sLastPageIndex: 2 [2,3]
+ 2015-02-27 17:20:20.486 BDStockClient[2151:234573] offset: 2.946875 sLastPageIndex: 2 [2,3]
+ 2015-02-27 17:20:20.503 BDStockClient[2151:234573] offset: 2.964062 sLastPageIndex: 2 [2,3]
+ 2015-02-27 17:20:20.520 BDStockClient[2151:234573] offset: 2.976562 sLastPageIndex: 2 [2,3]
+ 2015-02-27 17:20:20.536 BDStockClient[2151:234573] offset: 2.985938 sLastPageIndex: 2 [2,3]
+ 2015-02-27 17:20:20.553 BDStockClient[2151:234573] offset: 2.992188 sLastPageIndex: 2 [2,3]
+ 2015-02-27 17:20:20.570 BDStockClient[2151:234573] offset: 2.996875 sLastPageIndex: 2 [2,3]
+ 
+ 2015-02-27 17:20:20.586 BDStockClient[2151:234573] offset: 3.000000 sLastPageIndex: 2 [3,3]
+*/
 
 #pragma mark - BDSNewsTagBarDelegate
 - (void)onSelectTagAtIndex:(NSInteger)index {
